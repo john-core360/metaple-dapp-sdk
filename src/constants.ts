4d4b8c6a2b3ad3e0870b5d3432455c44076ed1e3
robin-core360 <robin.rajput@360coreinc.com>
2022-07-22 22:42:59 +0530

    first commit

--- src/constants.ts
+++ src/constants.ts
@@ -19,9 +19,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const FACTORY_ADDRESS = '0xFEbDEfCE883f1fF3D8f02fC699f337E4f0DF4271'
 
-export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+export const INIT_CODE_HASH = '0xfd9a1f6f7defb4bcb3c7e4ecad03885df669d5ed5c6d374edd85d58fbe171267'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
